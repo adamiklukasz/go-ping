1fc9d1e0c57186e6c2b0903e15708d0556206c2a
Adamik Lukasz <lukasz.adamik@solarwinds.cloud>
2020-10-12 13:39:47 +0200

    Changed API for PingN

--- ping.go
+++ ping.go
@@ -4,13 +4,25 @@ import (
 	"time"
 )
 
+type Stats struct {
+	Min time.Duration
+	Max time.Duration
+	Avg time.Duration
+}
+
 // returns one ping duration for a specific address
 func Ping(targetAddr string) (time.Duration, error) {
 	return parsePingCmd(targetAddr)
 }
 
 // returns average ping durations for a specific address
-func PingN(targetAddr string, count int, interval time.Duration) (time.Duration, error) {
+func PingN(targetAddr string, count int, interval time.Duration) (Stats, error) {
+	s := Stats{
+		Min: 1 * time.Hour,
+		Max: 0,
+		Avg: 0,
+	}
+
 	if count < 1 {
 		panic("count should be a positive number")
 	}
@@ -20,13 +32,23 @@ func PingN(targetAddr string, count int, interval time.Duration) (time.Duration,
 	for i := 0; i < count; i++ {
 		d, err := Ping(targetAddr)
 		if err != nil {
-			return 0, nil
+			return s, nil
 		}
 
 		sum += d
 
+		if s.Min > d {
+			s.Min = d
+		}
+
+		if s.Max < d {
+			s.Max = d
+		}
+
 		time.Sleep(interval)
 	}
 
-	return sum / time.Duration(count), nil
+	s.Avg = sum / time.Duration(count)
+
+	return s, nil
 }
